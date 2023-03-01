bc1ad45962fbbf16caf72f185dc754a32087482c
nachod14 <125494454+nachod14@users.noreply.github.com>
2023-03-01 13:06:47 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,11 @@
+
+function ver(n) {
+    document.getElementById("subseccion"+n).style.display="block"
+    }
+function ocultar(n) {
+    document.getElementById("subseccion"+n).style.display="none"
+    }
+function ver2(n) {
+    document.getElementById("subseccion2"+n).style.display="block"
+    } 
+onclick="ver(1)"
